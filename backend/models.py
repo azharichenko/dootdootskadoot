3ce256d40b8eeda46b6071fa488e5b47f160e6f3
Hayden Stec <hrs24@pitt.edu>
2018-10-06 07:13:04 -0400

    Added nullablefield

--- backend/models.py
+++ backend/models.py
@@ -45,7 +45,7 @@ class Doctor(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=50)
-    twilio_number = models.CharField(max_length=50)
+    twilio_number = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return "Dr. " + self.first_name + " " + self.last_name + " ID: " + self.id
--- client_portal/views.py
+++ client_portal/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from backend.models import  *
 
 def patient_information(request, slug):
     # slug == visit.id
-
+    visit = Visit.objects.filter(id=slug).first()
     data = {
         'patient': {
-            'name': "John" # visit.patient.last_name
-        }
+            'name': visit.patient.last_name + ", " + visit.patient.first_name
+        },
+        'diagnosis': {
+            'name': visit.diagnosis.diagnosis_name + "\n",
+            'description': visit.diagnosis.diagnosis_description
+        },
+        'treatment': {
+            'prescription': visit.treatment.prescription + "\nHow many days of treatment: " + visit.treatment.duration +
+            "\nDosage: " + visit.treatment.dosage + "\nTime Between Usage: " + visit.treatment.cycle
+        },
     }
     return render(request, template_name='client.html', context=data)
